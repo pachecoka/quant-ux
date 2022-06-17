c7487b3af5ed16336d17752587f7b573531b4659
Karolina Pacheco <38144404+pachecoka@users.noreply.github.com>
2022-06-16 21:15:59 -0300

    fix typos on animations doc

--- src/help/en/animations.js
+++ src/help/en/animations.js
@@ -4,9 +4,9 @@ export let texts = [
         "name":"Animations",
         "title": "Animations",
         "body": `
-            Quant-UX supports two kind of annimations. Widget animations change the style of a widget, depending on the
+            Quant-UX supports two kind of animations. Widget animations change the style of a widget, depending on the
             user interactions. For instance you can define a hover color of a button, or an error color for a text box.
-            In addtion, you can also create screen animations, that are shown once a screen is loaded. These include entrance
+            In addition, you can also create screen animations, that are shown once a screen is loaded. These include entrance
             animations, such as slide in effects, and more complex animations that are defined for every widget.
 
             
@@ -35,7 +35,7 @@ export let texts = [
                 "title": "Screen Animations",
                 "body": `
                     Before you create a screen animation, you have to create a link between a source (widget or screen) 
-                    and a target screen. Once you have created the link, select the souce element. In the properties panel,
+                    and a target screen. Once you have created the link, select the source element. In the properties panel,
                     you can now select in the <strong>Action</strong> section 
                     the <strong><span class="mdi mdi-close"/> No Animation</strong> button. A popup will show up,
                     that lets you define how the new screen is animated, for instance if it is faded or slided in. 
@@ -43,7 +43,7 @@ export let texts = [
                     <br>  <br>
                     You can also create dedicated loading animations that will animate each widget separately. 
                     Select a screen, and press the <strong><span class="mdi mdi-video" /> Animation</strong> button. 
-                    The animation composer will show up, which let's you define for each widget a animation 
+                    The animation composer will show up, which let's you define for each widget an animation 
                     time line. You can set the start and end of the animation. Press the 
                     <span class="mdi mdi-close"/> icon to select the type of animation.
 
@@ -59,8 +59,8 @@ export let texts = [
                 "title": "Screen Transforms",
                 "body": `
                     If you want more advanced animations you can use the <strong>Animation</strong> links. These 
-                    special kind of links create under the hoods for every widget a transorm animation t
-                    hat is shown when the screen is loaded. The best way to work with animation links is to:
+                    special kind of links create under the hoods for every widget a transorm animation that 
+                    is shown when the screen is loaded. The best way to work with animation links is to:
                     <ol>
                         <li>
                             Create a screen with all elements as the initial state.
